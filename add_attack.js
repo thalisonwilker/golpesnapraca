17acf73e91808505a35c10b96e7fd2172fe08314
Thalyson Wilker <thalison.wilker@gmail.com>
2022-06-29 12:25:49 -0300

    atualiza o contador de objetos

--- add_attack.js
+++ add_attack.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const dataDirSrc = "./data.js";
 
 const attackTemplate = {
-  "id": 1,
   "titile": "Urubu do PIX",
   "banner_url": "https://i.imgur.com/XqQZQ.jpg",
   "description": "Urubu do PIX é um golpe do Urubu, que causa dano ao oponente e aumenta o dano de seu próximo golpe.",
@@ -16,5 +15,19 @@ fs.readFile(dataDirSrc, 'utf8', (err, data) => {
     console.error(err);
     return;
   }
-  console.log(data);
+  const dataObj = JSON.parse(data);
+  let lastAttack = dataObj[dataObj.length - 1]
+  let id = 1
+  if(lastAttack){
+    id = lastAttack.id + 1
+  }
+  attackTemplate.id = id;
+  dataObj.push(attackTemplate);
+  fs.writeFile(dataDirSrc, JSON.stringify(dataObj, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Attack ${id} added!`);
+  });
 });
