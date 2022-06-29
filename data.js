f29683ca32a1d0a72452092b80f41ade01d736aa
Thalyson Wilker <thalison.wilker@gmail.com>
2022-06-29 12:26:55 -0300

    adicionar o golpe Urubu do PIX

--- data.js
+++ data.js
@@ -1,3 +1,13 @@
 [
-    
+  {
+    "titile": "Urubu do PIX",
+    "banner_url": "https://i.imgur.com/XqQZQ.jpg",
+    "description": "Urubu do PIX é um golpe do Urubu, que causa dano ao oponente e aumenta o dano de seu próximo golpe.",
+    "type": "normal",
+    "tags": [
+      "pix",
+      "whatsapp"
+    ],
+    "id": 1
+  }
 ]
